62ec5b1ca7ecb3d2321749d205372087fb899558
gokhanbirkin <gkandth@gmail.com>
2022-03-22 17:41:03 +0300

    added cubit

--- lib/features/searchPage/view/search_view.dart
+++ lib/features/searchPage/view/search_view.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:vbt_food_challange/features/searchPage/viewModel/search_cubit.dart';
 import 'package:vbt_food_challange/product/widgets/bottom_navbar.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchPageView extends StatelessWidget {
   const SearchPageView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => SearchPageCubit(),
+      child: BlocConsumer<SearchPageCubit, SearchPageState>(
+        listener: (context, state) {
+          if (state is SearcPageErrorState) {}
+        },
+        builder: (context, state) {
+          return buildScaffold(context, state);
+        },
+      ),
+    );
+  }
+
+  Scaffold buildScaffold(context, state) {
     return Scaffold(
       body: Center(
         child: Text('SearchPageView'),
---
+++ lib/features/searchPage/viewModel/search_cubit.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+
+class SearchPageCubit extends Cubit<SearchPageState> {
+  SearchPageCubit() : super(SearchPageInitial());
+}
+
+abstract class SearchPageState {}
+
+class SearchPageInitial extends SearchPageState {}
+
+class SearchPageLoadingState extends SearchPageState {}
+
+class SearcPageErrorState extends SearchPageState {}
